6cad6ba64e0d3f94b1e04f95cfb6fb5f2591c199
Richypiants <48888991+Richypiants@users.noreply.github.com>
2024-03-14 16:07:22 +0100

    Pattern logic continuation

--- src/main/java/it/polimi/ingsw/gc12/ServerModel/PatternCondition.java
+++ src/main/java/it/polimi/ingsw/gc12/ServerModel/PatternCondition.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.gc12.Utilities.Resource;
 import it.polimi.ingsw.gc12.Utilities.Triplet;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 //TODO: add documentation comments
 
@@ -23,25 +25,83 @@ public class PatternCondition implements PointsCondition {
     //FIXME: interfaces only allow public methods...
     public int numberOfTimesSatisfied(Card thisCard, InGamePlayer target) {
         //TODO: pattern match logic here
-        target.getOwnField().entrySet().stream()
-                .filter((entry) -> !entry.getKey().equals(new GenericPair<Integer, Integer>(0, 0)))
-                .filter((entry) -> entry.getValue()
-                        .getCenterBackResources()
-                        .getFirst()
-                        .equals(condition.getFirst().getZ())
+        return largestMaximumCompatibilityClass(
+                new ArrayList<PlayableCard>(target.getOwnField().entrySet().stream()
+                        .filter((entry) -> !entry.getKey().equals(new GenericPair<Integer, Integer>(0, 0)))
+                        .filter((entry) -> entry.getValue()
+                                .getCenterBackResources()
+                                .getFirst()
+                                .equals(condition.getFirst().getZ())
+                        )
+                        .filter((entry) -> getConditionParameters().subList(1, condition.size())
+                                .stream()
+                                .map((a) -> target.getOwnField().get(
+                                        new GenericPair<Integer, Integer>(
+                                                entry.getKey().getX() + a.getX(),
+                                                entry.getKey().getY() + a.getY()
+                                        )
+                                ).getCenterBackResources().getFirst().equals(a.getZ()))
+                                .reduce(true, (a, b) -> a && b)
+                        )
+                        .map(Map.Entry::getValue)
+                        .collect(Collectors.toList())
                 )
-                .filter((entry) -> getConditionParameters().subList(1, condition.size())
-                        .stream()
-                        .map((a) -> target.getOwnField().get(
-                                new GenericPair<Integer, Integer>(
-                                        entry.getKey().getX() + a.getX(),
-                                        entry.getKey().getY() + a.getY()
-                                )
-                        ).getCenterBackResources().getFirst().equals(a.getZ()))
-                        .reduce(true,
-                                (a, b) -> a && b)
-                ).algoritmoDellAlbero();
+        );
+    }
 
-        return 0;
+    public int largestMaximumCompatibilityClass(ArrayList<PlayableCard> cards) {
+        ArrayList<ArrayList<PlayableCard>> frontier = new ArrayList<ArrayList<PlayableCard>>();
+        ArrayList<ArrayList<PlayableCard>> result = new ArrayList<ArrayList<PlayableCard>>();
+        int nodesInLastLevel = 1, depth = 1;
+
+        frontier.add(cards);
+
+        while (!frontier.isEmpty()) {
+            for (int i = 0; i < nodesInLastLevel; i++) {
+                ArrayList<PlayableCard> tmp = frontier.removeFirst();
+                frontier.add(new ArrayList<PlayableCard>(tmp.subList(1, tmp.size())));
+                frontier.add(new ArrayList<PlayableCard>(
+                        tmp.removeIf(
+                                () ->
+                        )
+                );
+            }
+
+            for (int i = 0; i < frontier.size() - 1; i++) {
+                for (int j = i + 1; j < frontier.size(); j++) {
+                    if (frontier.get(j).containsAll(frontier.get(i))) {
+                        frontier.remove(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < frontier.size() - 1; i++) {
+                for (int j = 0; j < result.size(); j++) {
+                    if (frontier.get(j).containsAll(frontier.get(i))) {
+                        frontier.remove(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < frontier.size(); i++) {
+                //FIXME: è orribile... functional?
+                if (cards.indexOf(frontier.get(i).get(frontier.get(i).size() - 2)) < depth) {
+                    result.add(frontier.remove(i));
+                    i--;
+                }
+            }
+
+            nodesInLastLevel = frontier.size();
+            depth++;
+        }
+
+        return result.stream()
+                .mapToInt(ArrayList::size)
+                .max()
+                .getAsInt();
     }
 }
