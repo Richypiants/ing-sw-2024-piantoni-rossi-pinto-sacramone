42fa1db2b0cf8bbd953e333d4dd1968f79027b22
Jacopo Sacramone <jacopo.sacramone@mail.polimi.it>
2024-03-19 16:54:21 +0100

    Test for Card, Field (Problems), Game (JSON PARSER)

--- src/test/java/it/polimi/ingsw/gc12/ServerModel/CardTest.java
+++ src/test/java/it/polimi/ingsw/gc12/ServerModel/CardTest.java
@@ -1,16 +1,31 @@
 package it.polimi.ingsw.gc12.ServerModel;
 
+import it.polimi.ingsw.gc12.Utilities.Resource;
 import org.junit.jupiter.api.Test;
+
+import java.util.EnumMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardTest {
 
     @Test
-    void setShownSide() {
-
-    }
-
-    @Test
     void awardPoints() {
 
+        Resource[][] corner = {{Resource.WOLF, Resource.WOLF}, {Resource.WOLF, Resource.WOLF},
+                {Resource.WOLF, Resource.WOLF}, {Resource.WOLF, Resource.WOLF}};
+
+        ResourceCard c1 = new ResourceCard(1, 4, null, null, corner, new EnumMap<>(Resource.class));
+
+        Player player = new Player("SACRI");
+
+        InGamePlayer playerGame = new InGamePlayer(player);
+
+        System.out.print("value: " + c1.awardPoints(playerGame));
+
+        assertEquals(c1.awardPoints(playerGame), 4);
     }
 }
+
+// Test
+// - awardPoints() -> PASSED
--- src/test/java/it/polimi/ingsw/gc12/ServerModel/GameTest.java
+++ src/test/java/it/polimi/ingsw/gc12/ServerModel/GameTest.java
@@ -2,15 +2,44 @@ package it.polimi.ingsw.gc12.ServerModel;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class GameTest {
 
     @Test
     void nextPlayer() {
 
+        Player player = new Player("SACRI");
+        InGamePlayer playerGame = new InGamePlayer(player);
+        GameLobby lobby = new GameLobby(4, player);
+        Game game = new Game(lobby);
+
+        game.nextPlayer();
+        assertEquals(game.getCurrentPlayer(), 1);
+
+    }
+
+    @Test
+    void getCurrentPlayer() {
+
+        Player player = new Player("SACRI");
+        InGamePlayer playerGame = new InGamePlayer(player);
+        GameLobby lobby = new GameLobby(4, player);
+        Game game = new Game(lobby);
+
+        assertEquals(game.getCurrentPlayer(), 0);
+
+        game.nextPlayer();
+        assertEquals(game.getCurrentPlayer(), 1);
+
+        game.nextPlayer();
+        assertEquals(game.getCurrentPlayer(), 2);
+
     }
 
     @Test
     void drawFrom() {
+
 
     }
 
