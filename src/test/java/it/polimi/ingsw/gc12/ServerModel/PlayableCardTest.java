9bbfc4a8acb23767ac9234f2ccdde0dd441ac3fc
federicopinto <federico.pinto@mail.polimi.it>
2024-03-21 15:21:36 +0100

    playableCard Test added

--- src/test/java/it/polimi/ingsw/gc12/ServerModel/PlayableCardTest.java
+++ src/test/java/it/polimi/ingsw/gc12/ServerModel/PlayableCardTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PlayableCardTest {
 
     @Test
-    void getCornerResource() {
+    void getCornerResource() { //OK
 
 
         HashMap<GenericPair<Integer, Integer>, Resource> resource = new HashMap<>();
@@ -27,15 +27,27 @@ class PlayableCardTest {
 
         ResourceCard c1 = new ResourceCard(1, 2, null, null, corner, new EnumMap<>(Resource.class));
 
-        assertEquals(Resource.SCROLL, c1.getCornerResource(Side.FRONT, 1, 1));
+        assertEquals(Resource.WOLF, c1.getCornerResource(Side.FRONT, 1, 1));
 
-        // TODO: WIP (corner è stato cambiato da piants da un array ad una HashMap)
 
 
     }
 
     @Test
-    void getCorners() {
+    void getCorners() { //OK
+
+        HashMap<GenericPair<Integer, Integer>, Resource> resource = new HashMap<>();
+        resource.put(new GenericPair<>(0, 0), Resource.WOLF);
+        resource.put(new GenericPair<>(1, 0), Resource.WOLF);
+        resource.put(new GenericPair<>(0, 1), Resource.WOLF);
+        resource.put(new GenericPair<>(1, 1), Resource.WOLF);
+        HashMap<Side, HashMap<GenericPair<Integer, Integer>, Resource>> corner = new HashMap<>();
+        corner.put(Side.FRONT, resource);
+        corner.put(Side.BACK, resource);
+
+        ResourceCard c1 = new ResourceCard(1, 2, null, null, corner, new EnumMap<>(Resource.class));
+
+        assertEquals(corner.get(Side.FRONT), c1.getCorners(Side.FRONT));
 
     }
 }
