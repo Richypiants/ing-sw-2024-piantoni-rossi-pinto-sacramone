68b3c9c8621c3c1a9fc16d3183877751b34a7fc4
Jacopo Sacramone <jacopo.sacramone@mail.polimi.it>
2024-04-20 15:35:37 +0200

    First two screens of the GUI and fix some problems

--- src/main/java/it/polimi/ingsw/gc12/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/gc12/HelloApplication.java
@@ -3,18 +3,25 @@ package it.polimi.ingsw.gc12;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, InterruptedException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                stage.close();
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/it/polimi/ingsw/gc12/HelloController.java
+++ src/main/java/it/polimi/ingsw/gc12/HelloController.java
@@ -1,25 +1,55 @@
 package it.polimi.ingsw.gc12;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void newPane(ActionEvent event) throws IOException {
+    Label StatusLabel;
+
+    @FXML
+    TextField NicknameField;
+
+    @FXML
+    Button startbutton;
+
+    @FXML
+    protected void newPane() throws IOException {
+        // RIvedere perchè non funziona alla prima ma alla seconda
+        startbutton.setOnAction(event -> {
+            Stage boh = (Stage) startbutton.getScene().getWindow();
+            boh.close();
+        });
+
         Parent nickname = FXMLLoader.load(getClass().getResource("Nickname.fxml"));
         Scene nicknameScene = new Scene(nickname);
         Stage stage = new Stage();
         stage.setScene(nicknameScene);
         stage.show();
     }
+
+    @FXML
+    protected void Yes() throws IOException {
+        if (NicknameField.getCharacters().length() > 0) {
+            StatusLabel.setText("Status: Nickname Ok - Logged in");
+        } else {
+            StatusLabel.setText("Inserire prima un Nickname");
+        }
+    }
+
+    @FXML
+    protected void No() throws IOException {
+        StatusLabel.setText("Status: Not logged in");
+    }
+
+
 }
