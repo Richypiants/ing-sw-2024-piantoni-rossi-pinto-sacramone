9c014fd9b63f60781033fd8324d98e700a12611e
rossi-matteo <90650095+rossi-matteo@users.noreply.github.com>
2024-04-29 17:53:53 +0200

    Formalizing the communication through Socket using localhost. Partially implemented the TUIView when showing lobbies. Modified some commands.

--- src/main/java/it/polimi/ingsw/gc12/Utilities/Resource.java
+++ src/main/java/it/polimi/ingsw/gc12/Utilities/Resource.java
@@ -4,7 +4,25 @@ package it.polimi.ingsw.gc12.Utilities;
 An enumeration of all the possible "resources" values in card corners and back-centers
  */
 public enum Resource {
-    NOT_A_CORNER, EMPTY, WOLF, BUTTERFLY, GRASS, MUSHROOM, INK, FEATHER, SCROLL;
+
+    NOT_A_CORNER(" ", -1), EMPTY(" ", 223),
+    WOLF("W", 32), BUTTERFLY("B", 207),
+    GRASS("G", 82), MUSHROOM("M", 88),
+    INK("I", 94), FEATHER("F", 94),
+    SCROLL("S", 94);
+
+    public final String SYMBOL;
+
+    public final int ANSI_COLOR;
+
+    /**
+     *  ANSI_COLOR: -1 is used as a placeholder swapped during the card rendering with the card background
+     *  */
+
+    Resource(String symbol, int ansiColor){
+        this.SYMBOL = symbol;
+        this.ANSI_COLOR = ansiColor;
+    }
 }
 
 
